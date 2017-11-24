4bf8a7c057aa2e6f2c90fd38c217d8fa9523b36c
sdrzymala <slawomirdrzymala@outlook.com>
2017-11-24 01:29:59 +0100

    Add the first version that was previously created

---
+++ FindTableVariables/FindTableVariables/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using FindTableVariables;
+
+namespace FindTableVariables
+{
+
+    class Program
+    {
+
+ 
+
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\GIT\database";
+            string delimeter = " > ";
+
+            Console.WriteLine("Start");
+
+            var tableVariable = FindTableVariables.ReturnTableVariableFromDirectory(path, delimeter);
+            var tableVariableFlattaned = FindTableVariables.ReturnTableVariableFromDirectory(path, delimeter, true);
+
+            File.WriteAllText("tableVariable_stats.txt", tableVariable);
+            File.WriteAllText("tableVariableFlattaned_stats.txt", tableVariable);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+
+}
